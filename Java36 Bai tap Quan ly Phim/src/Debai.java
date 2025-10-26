ec61227d4f3c8fa066e44883348753c98485d088
hoangndv <nguyenduongvuhoang1994@gmail.com>
2025-10-26 10:40:38 +0700

    first code

---
+++ Java36 Bai tap Quan ly Phim/src/Debai.java
@@ -0,0 +1,16 @@
+public class Debai {
+    /*
+    * xây dựng chương trình Java quản lý các bộ phim tại một Rạp chiếu phim.
+    * Một bộ phim bao gồm các thông tin: tên phim, nam sản xuất, hãng sản xuất, giá vé, ngày chiếu
+    * Hãng sản xuất bao gồm: tên hãng sản xuất, quốc gia
+    * Ngay chiếu bao gồm thông tin: ngày, tháng, năm
+    *
+    * Xây dựng các lớp và thực hiện các phương thức sau:
+    * a. Kiểm tra xem giá vé của một phim bất kỳ có rẻ hơn giá vé của một phim khác hay ko?
+    * b. Cho biết tên của hãng sản xuất phim
+    * c. Cho biết giá vé cu phim khi có khuyến mãi (ví dụ sau khi khuyến ma 10%, 20%)
+    *
+    * Lưu : Viết lớp Test để kiểm tra Constructor và các phương thức
+    *
+    * */
+}
